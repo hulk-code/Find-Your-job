9611de2d3dadd815cbe0e766e949e13e891ed3ea
Akash <shoaibsarkarakash019@gmail.com>
2023-11-04 21:19:07 +0600

    Create navber

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Navber from "../Component/Pages/SharedPage/Navber/Navber";
 
 
 const Root = () => {
     return (
         <div>
+            <Navber></Navber>
             <Outlet></Outlet>
         </div>
     );
