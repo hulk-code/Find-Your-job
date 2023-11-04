d6f303f312e37a1a4a8a2d2daf604426b0e77469
Akash <shoaibsarkarakash019@gmail.com>
2023-11-04 21:31:00 +0600

    Create navber routes and make one active

--- src/Component/Pages/SharedPage/Navber/Navber.jsx
+++ src/Component/Pages/SharedPage/Navber/Navber.jsx
@@ -1,24 +1,26 @@
 
+// Home,Add job,My posted jobs,My Bids,Bid Requests
+
+import { NavLink } from "react-router-dom";
 
 const Navber = () => {
+    const navberRoutes=<>
+    <li><NavLink to='/'>Home</NavLink></li>
+    <li><NavLink to='/addjob'>Add_Job</NavLink></li>
+    <li><NavLink to='/mypostedjobs'>My_posted_jobs</NavLink></li>
+    <li><NavLink to='/,mybids'>My_Bids</NavLink></li>
+    <li><NavLink to='/bidrequests'>Bid_Requests</NavLink></li>
+    </>
     return (
         <div>
-            <div className="navbar bg-base-100">
+            <div className="navbar bg-base-100 font-bold">
   <div className="navbar-start">
     <div className="dropdown">
       <label tabIndex={0} className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 ">
+        {navberRoutes}
       </ul>
     </div>
     <div className="avatar">
@@ -30,17 +32,7 @@ const Navber = () => {
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li tabIndex={0}>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+      {navberRoutes}
     </ul>
   </div>
   <div className="navbar-end">
